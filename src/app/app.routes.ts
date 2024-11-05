d78e7ac95b4a70ca344be20264470debde288ca9
Christopher Ortega <chriiswho@gmail.com>
2024-11-04 17:54:23 -0700

    Added dashboard children routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { LoginComponent } from './presentation/pages/login/login.component';
 import { DashboardComponent } from './presentation/pages/dashboard/dashboard.component';
 import { EmployeesComponent } from './presentation/pages/dashboard/components/employees/employees.component';
 import { RecentComponent } from './presentation/pages/dashboard/components/recent/recent.component';
+import { MessagesComponent } from './presentation/pages/dashboard/components/messages/messages.component';
+import { ActivitiesComponent } from './presentation/pages/dashboard/components/activities/activities.component';
+import { SectorsComponent } from './presentation/pages/dashboard/components/sectors/sectors.component';
+import { WarehouseComponent } from './presentation/pages/dashboard/components/warehouse/warehouse.component';
 
 export const routes: Routes = [
 
@@ -21,6 +25,18 @@ export const routes: Routes = [
       },
       {
         path: 'empleados', component: EmployeesComponent
+      },
+      {
+        path: 'mensajes', component: MessagesComponent
+      },
+      {
+        path: 'actividades', component: ActivitiesComponent
+      },
+      {
+        path: 'sectores', component: SectorsComponent
+      },
+      {
+        path: 'almacen', component: WarehouseComponent
       }
     ]
   },
