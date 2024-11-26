a8cbe5329abd1ac45ebb38f8c2fe8936ab948c93
Christopher Ortega <chriiswho@gmail.com>
2024-11-25 17:12:36 -0700

    Fixed logout repository

--- src/app/infrastructure/auth/auth.repository.implementation.ts
+++ src/app/infrastructure/auth/auth.repository.implementation.ts
@@ -20,17 +20,32 @@ export class AuthRepositoryImplementation implements AuthRepository {
   ){}
 
   login(numeroEmpleado: number, empleadoPassword: string): Observable<Employee>{
+    return this.http.post<Employee>(`${this.apiUrl}/login`, {numeroEmpleado, empleadoPassword}, {
+      withCredentials: true,              // With credentials is for include the cookies.
+    })
+    /*
     return this.http.post<Employee>(`${this.apiUrl}/login`, { numeroEmpleado, empleadoPassword }).pipe(
       tap(employee => {
         localStorage.setItem(this.employeeKey, JSON.stringify(employee));
       })
     )
+    */
   }
 
   logout(): void {
-    this.http.post<any>(`${this.apiUrl}/logout`, null);
-    localStorage.removeItem(this.employeeKey);
-    this.router.navigate(['/login']);
+
+    this.http.post<void>(`${this.apiUrl}/logout`, null, { withCredentials: true}).subscribe({
+      next: () => {
+        this.router.navigate(['/login']);
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
+
+  isAuthenticated(): void {
+
   }
 
   getEmployee(): Employee | null {
